1ee6bbd94c614845b6f95ddc3fe22ebcfdb9947c
Kennata <kenata966@gmail.com>
2026-06-10 19:19:22 +0700

    mengubah alamat URL yang ada dalam file FoodLyAPIClient

--- my-FoodLy-app/src/services/FoodLyAPIClient.jsx
+++ my-FoodLy-app/src/services/FoodLyAPIClient.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const FoodlyApiClient = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://foodly-backend-5mci.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json"
